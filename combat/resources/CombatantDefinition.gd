ce16ee976128b9becb9098b5839f06eaad4ff9c8
HopeMetal <liudasdy@gmail.com>
2023-12-07 14:22:17 +0200

    Added export groups to CombatantDefinition

--- combat/resources/CombatantDefinition.gd
+++ combat/resources/CombatantDefinition.gd
@@ -1,13 +1,15 @@
 extends Resource
 class_name CombatantDefinition
 
-@export_category("Stats")
+
 @export var name = ""
-@export_range(1, 2, 1, "or_greater") var max_hp = 1
+@export_group("Class")
 @export_enum("Melee", "Ranged", "Magic") var class_t = 0
 @export_enum("Ground", "Flying", "Mounted") var class_m = 0
+@export_group("Stats")
+@export_range(1, 2, 1, "or_greater") var max_hp = 1
 @export_range(1, 3, 1, "or_greater") var movement = 3
 @export_range(1, 2, 1, "or_greater") var initiative = 1
-@export_category("Visual")
+@export_group("Visual")
 @export var icon: Texture2D
 @export var map_sprite: Texture2D
